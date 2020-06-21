13feaf7515561998caf7c29e34fa04303e1b81be
harshdonga <harshdonga99@gmail.com>
2020-06-21 23:49:42 +0530

    2nd

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth import authenticate. login, logout
+from djang.contrib.auth.models import User
 def index(request):
     return HttpResponse('<em>In Candidate Arena</em>')
+
+
+def login(request):
+    pass
+
+
+def logout(request):
+    pass
